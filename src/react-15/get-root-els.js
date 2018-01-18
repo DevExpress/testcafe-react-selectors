a7388997943c7f81329c5cda3522b6f4b4ba4914
Boris Kirov <kirovbs@mail.ru>
2018-01-18 13:28:55 +0300

    Fix remarks

--- src/react-15/get-root-els.js
+++ src/react-15/get-root-els.js
@@ -9,7 +9,8 @@ function getRootElsReact15 () {
                          Object.keys(rootEls[0]).some(prop => {
                              const rootEl = rootEls[0];
 
-                             //NOTE: server rendering in React 16 also adds data-reactroot attribute
+                             //NOTE: server rendering in React 16 also adds data-reactroot attribute, but
+                             //the _hostContainerInfo field doesn't exists in react 16, so need an extra check for it.
                              return /^__reactInternalInstance/.test(prop) && !!rootEl[prop]._hostContainerInfo;
                          });
 
