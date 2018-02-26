ed1541b21be7b5e12576e2f034b9c342081643a1
Boris Kirov <kirovbs@mail.ru>
2018-02-26 10:08:34 +0300

    Fixed the React 15 roots detection

--- src/react-15/get-root-els.js
+++ src/react-15/get-root-els.js
@@ -9,9 +9,9 @@ function getRootElsReact15 () {
                          Object.keys(rootEls[0]).some(prop => {
                              const rootEl = rootEls[0];
 
-                             //NOTE: server rendering in React 16 also adds data-reactroot attribute, but
-                             //the _hostContainerInfo field doesn't exists in react 16, so need an extra check for it.
-                             return /^__reactInternalInstance/.test(prop) && !!rootEl[prop]._hostContainerInfo;
+                             //NOTE: server rendering in React 16 also adds data-reactroot attribute, we check existing the
+                             //alternate field because it doesn't exists in React 15.
+                             return /^__reactInternalInstance/.test(prop) && !rootEl[prop].hasOwnProperty('alternate');
                          });
 
     return checkRootEls && rootEls || [];
