b1ddd4095dabc60bc4dcb585081f0bac5bdc8189
Boris Kirov <kirovbs@mail.ru>
2018-05-24 13:41:01 +0300

    Improved the waiting mechanism in functional tests

--- test/helpers/service-util.js
+++ test/helpers/service-util.js
@@ -3,6 +3,15 @@ import { ClientFunction } from 'testcafe';
 import { waitForReact } from '../../';
 
 export async function loadApp (version) {
-    await ClientFunction(() => window.loadApp(version), { dependencies: { version } })();
+    await ClientFunction(() => {
+        if (window.loadApp)
+            window.loadApp(version);
+
+        else {
+            setTimeout(() => {
+                window.loadApp(version);
+            }, 200);
+        }
+    }, { dependencies: { version } })();
     await waitForReact();
 }
