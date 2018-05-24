aa018e10977833c8bf88648074536cea15fbd086
Boris Kirov <kirovbs@mail.ru>
2018-05-24 14:01:26 +0300

    Try to improve the waiting mechanism for server rendering

--- test/helpers/service-util.js
+++ test/helpers/service-util.js
@@ -4,14 +4,19 @@ import { waitForReact } from '../../';
 
 export async function loadApp (version) {
     await ClientFunction(() => {
-        if (window.loadApp)
-            window.loadApp(version);
-
-        else {
-            setTimeout(() => {
+        return new Promise(resolve => {
+            if (window.loadApp) {
                 window.loadApp(version);
-            }, 200);
-        }
+                resolve();
+            }
+
+            else {
+                setTimeout(() => {
+                    window.loadApp(version);
+                }, 200);
+            }
+        });
     }, { dependencies: { version } })();
-    await waitForReact();
+
+    await waitForReact(3e4);
 }
